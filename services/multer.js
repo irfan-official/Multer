56bfb7115e0278fae6487b1c4e2606cb3b7cdabd
irfan-official <irfanofficial.work@gmail.com>
2025-03-07 11:03:22 +0600

    updates

--- services/multer.js
+++ services/multer.js
@@ -2,21 +2,21 @@ import multer from "multer";
 import path from "path";
 
 // Disk storage
-const Diskstorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    let ext = path.extname(file.originalname).split(".");
-    console.log(ext);
-    let filePath = `./public/images/uploads/${ext[1]}`;
-    cb(null, filePath);
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
-    );
-  },
-});
+// const Diskstorage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     let ext = path.extname(file.originalname).split(".");
+//     console.log(ext);
+//     let filePath = `./public/images/uploads/${ext[1]}`;
+//     cb(null, filePath);
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(
+//       null,
+//       file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+//     );
+//   },
+// });
 // Memory Storage
 const Memorystorage = multer.memoryStorage();
 const upload = multer({
